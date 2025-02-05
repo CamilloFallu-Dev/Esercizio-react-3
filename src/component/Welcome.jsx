6f444b7fffae6781686f4a4ff512e5ab8d833723
Camillo Fallù <camillofallu9920@gmail.com>
2025-02-05 11:53:02 +0100

    fix esercizio

--- src/component/Welcome.jsx
+++ src/component/Welcome.jsx
@@ -9,12 +9,12 @@ function Welcome({ name, age }) {
       <p>
         <Eta age={age} />
       </p>
-      <Message age={age} />
       <div className="flex gap-4 p-4 bg-blue-300 font-bold">
         {age > 18 && <Eta age={age} />}
         {age && <Eta age={age} />}
         {age > 18 && age < 65 && <Eta age={age} />}
         {age > 18 && name === "John" && <Eta age={age} />}
+        <Message age={age} />
       </div>
     </div>
   );
